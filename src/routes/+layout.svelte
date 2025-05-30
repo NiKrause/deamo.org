d660b896449a149a441f56c2fd6a30b050e4b276
Nico Krause <post@nicokrause.com>
2025-05-30 13:59:27 +0500

    adding version to title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,11 @@
 	import '../app.css';
 
 	let { children } = $props();
+	const blogName = 'DeAmo.org'; // You can change this to your preferred name
 </script>
+
+<svelte:head>
+	<title>{blogName} v{version}</title>
+</svelte:head>
 
 {@render children()}
