8cc858328b4a87d4a935627d68ee92a39e1a77a8
Nico Krause <post@nicokrause.com>
2025-05-30 13:57:05 +0500

    adding ipfs publish and version numbers

---
+++ ipfs-publish.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Run the ipfs add command and capture the output
+output=$(ipfs add -r dist)
+
+# Extract the CID using awk or cut
+cid=$(echo "$output" | tail -n 1 | awk '{print $2}')
+echo "latest IPFS CID $cid"
+
+# Run the ipfs name publish command with the extracted CID
+ipfs name publish --key=deam.org /ipfs/$cid
+echo "IPFS name orbitblog updated with CID $cid"
+# Update the vercel.json file with the new CID
+# sed -i '' "s|/ipfs/[^\"}]*|/ipfs/$cid|g" vercel.json
+
+# Execute the docker-compose command on the remote server
+# ssh -t root@ipfs.le-space.de "cd docker/ipfs/willschenk && docker-compose exec ipfs ipfs add $cid"
+# echo "IPFS CID $cid added to ipfs.le-space.de"
+# Pin the CID to ipfs.le-space.de
+ssh -t root@ipfs.le-space.de "su ipfs -c 'ipfs pin add $cid'"
+echo "IPFS CID $cid pinned to ipfs.le-space.de"
+
+
+# echo the result of name resolve should be the same as the cid
+result=$(ssh -t root@ipfs.le-space.de "su ipfs -c 'ipfs name resolve --nocache /ipns/k51qzi5uqu5djjnnjgtviql86f19isjyz6azhw48ovgn22m6otstezp2ngfs8g'" | tr -d '\r' | tr -d '\n')
+
+# Debug with hexdump to see exactly what characters we're getting
+echo "Result raw:"
+echo "$result" | hexdump -C
+echo "CID raw:"
+echo "$cid" | hexdump -C
+
+if [ "$result" == "/ipfs/$cid" ]; then
+    echo "$(tput setaf 2)IPFS name resolve result matches CID $cid$(tput sgr0)"
+else
+    echo "$(tput setaf 1)IPFS name resolve result does not match CID $cid$(tput sgr0)"
+fi
+
+
+# echo "IPFS PIN added to follow ipns"
+# Get the current version from package.json
+version=$(node -p "require('./package.json').version")
+
+# Git commands
+# git add vercel.json
+git commit -m "Update IPFS CID to $cid for version $version"
+git tag -a "v$version" -m "Version $version"
+git push origin main
+git push origin --tags
+
+echo "Changes committed and pushed to GitHub. Tagged as v$version"
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
+import { readFileSync } from 'fs';
+import path from 'path';
+
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = readFileSync(file, 'utf8');
+const pkg = JSON.parse(json);
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	define: {
+		__APP_VERSION__: JSON.stringify(pkg.version),
+	}
 });
